f5acb4040deb4f95a53281366791e8f47e85ba62
iamngoni <imngonii@gmail.com>
2021-02-12 12:02:38 +0200

    conf

--- src/examples/example.ts
+++ src/examples/example.ts
@@ -1,6 +1,6 @@
 import HotRecharge from "../service/hotrecharge";
 
-const hotrecharge = new HotRecharge({email: "imngonii@gmail.com", password: 'Nickm@ng13'},);
+const hotrecharge = new HotRecharge({email: "email address", password: 'password'},);
 
 hotrecharge.pinlessRecharge(10, '0713700601').then(function (data) {
   console.log(data);
